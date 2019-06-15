479110b87eca7b70a343765d63b9d86e9403e633
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-15 11:33:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,17 @@
 def square_array(array)
+<<<<<<< HEAD
 squared=[] 
 i = 0  
+=======
+i = 1 
+>>>>>>> 26af06f81d61d15a930930edfefa671c57ff3a02
 while i < array.length do
   array[i] = array[i]**2
   i += 1
 end
+<<<<<<< HEAD
   return array 
+=======
+  
+>>>>>>> 26af06f81d61d15a930930edfefa671c57ff3a02
 end
